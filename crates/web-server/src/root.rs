434bdc571207a2ea6e8c6cae3d46fcb3f43ccd1e
ohmycloud <ohmycloudy@gmail.com>
2025-08-31 16:32:15 +0800

    validate the eamil

--- crates/web-server/src/root.rs
+++ crates/web-server/src/root.rs
@@ -1,9 +1,11 @@
 use axum::{
     Extension,
-    response::{Html, Redirect},
+    http::StatusCode,
+    response::{Html, IntoResponse, Redirect, Response},
 };
 use axum_extra::extract::Form;
 use serde::Deserialize;
+use validator::Validate;
 use web_pages::root;
 
 use crate::errors::CustomError;
@@ -15,19 +17,25 @@ pub async fn loader(Extension(pool): Extension<db::Pool>) -> Result<Html<String>
     Ok(Html(html))
 }
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Validate)]
 pub struct SignUp {
+    #[validate(email)]
     email: String,
 }
 
 pub async fn new_user_action(
     Extension(pool): Extension<db::Pool>,
     Form(form): Form<SignUp>,
-) -> Result<Redirect, CustomError> {
+) -> Result<Response, CustomError> {
+    if form.validate().is_err() {
+        return Ok((StatusCode::BAD_REQUEST, "Bad request").into_response());
+    }
     let client = pool.get().await?;
     let email = form.email;
     let _ = db::queries::users::create_user()
         .bind(&client, &email.as_str())
         .await?;
-    Ok(Redirect::to("/"))
+
+    // 303 redirect to users list
+    Ok(Redirect::to("/").into_response())
 }
