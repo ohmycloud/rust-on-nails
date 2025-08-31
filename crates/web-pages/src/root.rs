443810bcea5d24d34f92704356e9baaebfb94919
ohmycloud <ohmycloudy@gmail.com>
2025-08-31 21:51:07 +0800

    add sidebar, but render is not ok

--- crates/web-pages/src/root.rs
+++ crates/web-pages/src/root.rs
@@ -1,52 +1,89 @@
+use crate::{
+    layout::{Layout, SideBar},
+    render,
+};
+use daisy_rsx::*;
 use db::User;
 use dioxus::prelude::*;
 use web_assets::files::favicon_svg;
 
-use crate::{layout::Layout, render};
-
 pub fn index(users: Vec<User>) -> String {
     let page = rsx! {
         Layout {
-            title: "User Table",
-            table {
-                thead {
-                    tr {
-                        th { "ID" }
-                        th { "Email" }
-                    }
+            title: "Users Table",
+            selected_item: SideBar::Users,
+            BlankSlate {
+                heading: "Welcome To Your Application",
+                visual: favicon_svg.name,
+                description: "This is just the beginning",
+            }
+            Card {
+                class: "card-bordered mt-12 has-data-table",
+                CardHeader {
+                    class: "p-3 border-b",
+                    title: "Users"
                 }
-                tbody {
-                    for user in users {
-                        tr {
-                            td {
-                                img {
-                                    src: favicon_svg.name,
-                                    width: "16",
-                                    height: "16",
-                                }
-                                strong { "{user.id}" }
+                CardBody {
+                    class: "p-0",
+                    table {
+                        class: "table table-sm",
+                        thead {
+                            tr {
+                                th { "ID" }
+                                th { "Email" }
                             }
-                            td {
-                                "{user.email}"
+                        }
+                        tbody {
+                            for user in users {
+                                tr {
+                                    td {
+                                        strong {
+                                            "{user.id}"
+                                        }
+                                    }
+                                    td {
+                                        "{user.email}"
+                                    }
+                                }
                             }
                         }
                     }
                 }
             }
 
-            form {
-                action: "/new_user",
-                method: "POST",
-                label { r#for: "user_email", "Email:"}
-                input {
-                    id: "user_email",
-                    name: "email",
-                    r#type: "email",
-                    required: "true",
+            Card {
+                class: "card-bordered mt-12",
+                CardHeader {
+                    class: "p-3 border-b",
+                    title: "Add User"
                 }
-                button { "Submit" }
+                CardBody {
+                    class: "p-3",
+                    form {
+                        "hx-boost": "true",
+                        class: "flex flex-col",
+                        action: "/new_user",
+                        method: "POST",
+
+                        Input {
+                            input_type: InputType::Email,
+                            placeholder: "e.g. ian@test.com",
+                            help_text: "Please enter an email address",
+                            required: true,
+                            label: "Email",
+                            name: "email"
+                        }
+                        Button {
+                            class: "mt-4",
+                            button_type: ButtonType::Submit,
+                            button_scheme: ButtonScheme::Primary,
+                            "Submit"
+                        }
+                    }
+                }
             }
         }
     };
+
     render(page)
 }
